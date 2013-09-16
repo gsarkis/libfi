f4e0a4a55cdee2bcc2ed0f337b59509bd9dac373
Gabi Sarkis <gfsarkis@gmail.com>
2013-09-16 14:25:24 -0400

    use default overflow mode for README example

--- examples/example_simple.cpp
+++ examples/example_simple.cpp
@@ -5,8 +5,8 @@
 
 int main(int argc, char* argv[]) {
 
-  Fi::Fixed<8, 4, Fi::SIGNED, Fi::Wrap> a("3.14");
-  Fi::Fixed<8, 4, Fi::SIGNED, Fi::Wrap> b("13.14");
+  Fi::Fixed<8, 4, Fi::SIGNED> a("3.14");
+  Fi::Fixed<8, 4, Fi::SIGNED> b("1.14");
 
   std::cout << a*b << std::endl;
 
