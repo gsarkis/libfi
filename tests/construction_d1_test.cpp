1535bac7b2953071d44347c72a1b9bcce84bfd5c
Gabi Sarkis <gfsarkis@gmail.com>
2014-10-14 14:17:27 -0400

    more tests for bug #25

--- tests/construction_d1_test.cpp
+++ tests/construction_d1_test.cpp
@@ -9,15 +9,45 @@
 
 
 BOOST_AUTO_TEST_SUITE(construction_signed)
-typedef Fi::Fixed<16, 15, Fi::SIGNED, Fi::Throw> fi16s;
 
-BOOST_AUTO_TEST_CASE( u_from_string )
+BOOST_AUTO_TEST_CASE( s_from_string_4 )
 {
 
+	typedef Fi::Fixed<4, 3, Fi::SIGNED, Fi::Throw> fi4s;
+	CHECK_EQ( fi4s("0.375"), "0.375" );
+
+}
+
+BOOST_AUTO_TEST_CASE( s_from_string_16 )
+{
+
+	typedef Fi::Fixed<16, 15, Fi::SIGNED, Fi::Throw> fi16s;
 	CHECK_EQ( fi16s("0.043731689453125"), "0.043731689453125" );
 
 }
 
+
 BOOST_AUTO_TEST_SUITE_END()
 
 ////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE(construction_unsigned)
+
+BOOST_AUTO_TEST_CASE( u_from_string_4 )
+{
+
+	typedef Fi::Fixed<4, 3, Fi::UNSIGNED, Fi::Throw> fi4;
+	CHECK_EQ( fi4("0.375"), "0.375" );
+
+}
+
+BOOST_AUTO_TEST_CASE( u_from_string_16 )
+{
+
+	typedef Fi::Fixed<16, 15, Fi::UNSIGNED, Fi::Throw> fi16;
+	CHECK_EQ( fi16("0.043731689453125"), "0.043731689453125" );
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
