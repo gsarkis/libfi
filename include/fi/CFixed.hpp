42c88138df35d77d7e7f7b652cbbb9a21c63fc3d
François Leduc-Primeau <francoislp@gmail.com>
2016-06-21 14:41:50 +0200

    Adding a complex type - first draft

---
+++ include/fi/CFixed.hpp
@@ -0,0 +1,267 @@
+/*
+
+ *Copyright © 2016 Francois Leduc-Primeau
+
+ *This file is part of the C++ Fixed-Point Library (LibFi).
+
+ *LibFi is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+
+ *LibFi is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+
+ *You should have received a copy of the GNU General Public License
+ *along with LibFi.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+#ifndef FI_CFIXED_HPP
+#define FI_CFIXED_HPP
+
+namespace Fi {
+
+	/**\brief Complex number composed of two Fi::Fixed values with identical parameters
+	 *
+	 *\tparam W Total word length in bits, including the sign bit.
+	 *Must be less than or equal to 32.
+	 *\tparam F Number of fractional bits. Must be less than or equal to W.
+	 *\tparam S Signedness of the resulting type.
+	 *\tparam OF Overflow handling. It defaults to wrap-around.
+	 *\tparam R Rounding mode. It defaults to rounding towards zero.
+	 *
+	 *\see Fi::Fixed
+	 */
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF=Fi::Throw,
+	         template <typename> class R=Fi::Fix>
+	class CFixed {
+
+		/**
+		 *\brief Create a new CFixed object initialized to zero.
+		 */		
+		CFixed();
+
+		/**
+		 *\brief Copies a CFixed object.
+		 */
+		CFixed(const CFixed& a);
+
+		/**
+		 *\brief Creates a new CFixed object from a double-precision 
+		 *floating-point real part and imaginary part.
+		 *
+		 *\param realf Floating-point real part.
+		 *\param imagf Floating-point imaginary part.
+		 */
+		CFixed(double realf, double imagf);
+
+		/**
+		 *\brief Creates a new CFixed object from two Fixed objects.
+		 */
+		CFixed(const Fixed<W,F,S,OF,R>& real, const Fixed<W,F,S,OF,R>& imag);
+
+		/**
+		 *\brief Assignment operator.
+		 *\param that CFixed object to assign from.
+		 */
+		CFixed& operator=(const CFixed& that);
+
+		/**
+		 *\brief Equality comparison operator
+		 */
+		bool operator==(const CFixed& x) const;
+
+		/**
+		 *\brief Increment operator.
+
+		 *Increments the current value by \c x.
+
+		 *\param x Increment amount.
+		 */
+		CFixed& operator+=(const CFixed& x);
+
+		/**
+		 *\brief Decrement operator.
+
+		 *Decrements the current value by \c x.
+
+		 *\param x Decrement amount.
+		 */
+		CFixed& operator-=(const CFixed& x);
+
+		/**
+		 *\brief Multiply and store operator.
+
+		 *Multiplies the current value by \c x.
+
+		 *\param x Multiplicand.
+		 */
+		CFixed& operator*=(const CFixed& x);
+
+		/**
+		 *\brief Divide and store operator.
+
+		 *Divides the current value by \c x.
+
+		 *\param x Divisor.
+		 */
+		// CFixed& operator/=(const CFixed& x);		
+
+		/**
+		 *\brief Convert to \c std::complex<double>.
+
+		 *Converts the fixed-point complex number to std::complex<double>.
+
+		 *\sa CFixed::toString() for a higher-precision display method.
+		 *\return floating-point number.
+		 */
+		std::complex<double> toDouble() const;
+
+		/**
+		 * Returns the real part.
+		 */
+		Fixed real() const;
+
+		/**
+		 * Returns the imaginary part.
+		 */
+		Fixed imag() const;
+
+	private:
+
+		Fixed<W,F,S,OF,R> _real;
+		Fixed<W,F,S,OF,R> _imag;
+
+	}; // end class CFixed
+
+	/** \name Comparison operators */
+	//@{
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Equality check operator.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	inline bool operator==(const Fi::CFixed<W, F, S, OF, R>& x, 
+	                       const Fi::CFixed<W, F, S, OF, R>& y);
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Inequality check operator.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	inline bool operator!=(const Fi::CFixed<W, F, S, OF, R>& x, 
+	                       const Fi::CFixed<W, F, S, OF, R>& y);
+
+	//@}
+
+	/** \name Arithmetic operators */
+	//@{
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Addition operator.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator+(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y);
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Subtraction operator.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator-(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y);
+
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Negation operator.
+     
+	 *Negates the number.
+
+	 *\return The additive inverse of the number.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator-(const Fi::CFixed<W, F, S, OF, R>& a);
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Multiplication operator.
+	 */
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R>
+	operator*(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y);
+
+	/**
+	 *\relates Fi::CFixed
+	 *\brief Division operator.
+	 */
+	// template<std::size_t W, std::size_t F, Fi::Signedness S,
+	//          template <typename> class OF,
+	//          template <typename> class R>
+	// const Fi::CFixed<W, F, S, OF, R>
+	// operator/(const Fi::CFixed<W, F, S, OF, R>& x, 
+	//           const Fi::CFixed<W, F, S, OF, R>& y);
+
+	//@}	
+}
+
+namespace std {
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> real(const Fi::CFixed<W, F, S, OF, R>& x);
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> imag(const Fi::CFixed<W, F, S, OF, R>& x);
+	
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> abs(const Fi::CFixed<W, F, S, OF, R>& x);
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> arg(const Fi::CFixed<W, F, S, OF, R>& x);
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> norm(const Fi::CFixed<W, F, S, OF, R>& x);
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::CFixed<W, F, S, OF, R> conj(const Fi::CFixed<W, F, S, OF, R>& x);	
+	
+}
+
+#include "private/CFixed_priv.hpp"
+
+#endif
---
+++ include/fi/private/CFixed_priv.hpp
@@ -0,0 +1,280 @@
+/*
+
+ *Copyright © 2016 Francois Leduc-Primeau
+
+ *This file is part of the C++ Fixed-Point Library (LibFi).
+
+ *LibFi is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+
+ *LibFi is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+
+ *You should have received a copy of the GNU General Public License
+ *along with LibFi.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+#ifndef PRIVATE_FI_CFIXED_PRIV_HPP
+#define PRIVATE_FI_CFIXED_PRIV_HPP
+
+#include <cmath>
+
+namespace Fi {
+
+	// ---------- Member functions ----------
+	
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::CFixed()
+		: _real(Fixed<W,F,S,OF,R>()),
+		  _imag(Fixed<W,F,S,OF,R>())
+	{}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::CFixed(const CFixed<W,F,S,OF,R>& a)
+		: _real(a._real),
+		  _imag(a._imag)
+	{}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::CFixed(double realf, double imagf)
+	{
+		_real= Fixed<W,F,S,OF,R>(realf);
+		_imag= Fixed<W,F,S,OF,R>(imagf);
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::CFixed(const Fixed<W,F,S,OF,R>& real,
+	                               const Fixed<W,F,S,OF,R>& imag)
+		: _real(real),
+		  _imag(imag)
+	{}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::operator=(const CFixed<W, F, S, OF, R>& that) {
+		this->_real= that._real;
+		this->_imag= that._imag;
+		return *this;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::operator+=(const CFixed<W, F, S, OF, R>& that) {
+		_real+= that._real;
+		_imag+= that._imag;
+		return *this;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::operator-=(const CFixed<W, F, S, OF, R>& that) {
+		_real-= that._real;
+		_imag-= that._imag;
+		return *this;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W,F,S,OF,R>& CFixed<W, F, S, OF, R>::
+	operator*=(const CFixed<W,F,S,OF,R>& that) {
+		Fixed<W,F,S,OF,R> newReal= _real * that.real() - _imag * that.imag();
+		Fixed<W,F,S,OF,R> newImag= _real * that.imag() + _imag * that.real();
+		_real= newReal;
+		_imag= newImag;
+		return *this;
+	}
+		
+	
+	//TODO: multiplication with operands having different widths
+	// template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	//          template <typename> class OF,
+	//          template <typename> class R>
+	// template<std::size_t W2, std::size_t F2, Fi::Signedness S2, 
+	//          template <typename> class OF2,
+	//          template <typename> class R2>
+	// CFixed<W,F,S,OF,R>& CFixed<W, F, S, OF, R>::
+	// operator*=(const CFixed<W2, F2, S2, OF2, R2>& that) {
+	// 	// perform the operation using the maximum width and fractional
+	// 	// width of both operands
+	// }
+
+	// template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	//          template <typename> class OF=Fi::Throw,
+	//          template <typename> class R=Fi::Fix>
+	// CFixed<W, F, S, OF, R>::operator/=(const CFixed<W, F, S, OF, R>& that) {
+
+	// }
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S, 
+	         template <typename> class OF,
+	         template <typename> class R>
+	CFixed<W, F, S, OF, R>::toDouble() const {
+		std::complex<double> cdbl(_real.toDouble(), _imag.toDouble());
+		return cdbl;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fixed<W,F,S,OF,R> real() const {
+		return _real;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fixed<W,F,S,OF,R> imag() const {
+		return _imag;
+	}		
+
+
+	// ---------- Non-member function ----------
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	inline bool operator==(const Fi::CFixed<W, F, S, OF, R>& x, 
+	                       const Fi::CFixed<W, F, S, OF, R>& y)
+	{
+		return x.operator==(y);
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	inline bool operator!=(const Fi::CFixed<W, F, S, OF, R>& x, 
+	                       const Fi::CFixed<W, F, S, OF, R>& y)
+	{
+		return !(x==y);
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator+(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y)
+	{
+		CFixed<W,F,S,OF,R> ret(x);
+		ret+= y;
+		return ret;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator-(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y)
+	{
+		CFixed<W,F,S,OF,R> ret(x);
+		ret-= y;
+		return ret;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R> 
+	operator-(const Fi::CFixed<W, F, S, OF, R>& a)	{
+		CFixed<W,F,S,OF,R> ret();
+		ret._real-= a._real;
+		ret._imag-= a._imag;
+		return ret;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	const Fi::CFixed<W, F, S, OF, R>
+	operator*(const Fi::CFixed<W, F, S, OF, R>& x, 
+	          const Fi::CFixed<W, F, S, OF, R>& y) {
+		CFixed<W,F,S,OF,R> ret(x);
+		ret*= y;
+		return ret;
+	}
+
+	// template<std::size_t W, std::size_t F, Fi::Signedness S,
+	//          template <typename> class OF,
+	//          template <typename> class R>
+	// const Fi::CFixed<W, F, S, OF, R>
+	// operator/(const Fi::CFixed<W, F, S, OF, R>& x, 
+	//           const Fi::CFixed<W, F, S, OF, R>& y) {
+	// 	CFixed<W,F,S,OF,R> ret(x);
+	// 	ret/= y;
+	// 	return ret;
+	// }
+	
+} // end namespace Fi
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> std::real(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		return x._real;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> std::imag(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		return x._imag;
+	}
+	
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> std::abs(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		double result= sqrt(x.norm().toDouble());
+		return Fi::Fixed<W,F,S,OF,R>(result);
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> std::arg(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		double result= atan2(x.imag(),x.real());
+		return Fi::Fixed<W,F,S,OF,R>(result);
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::Fixed<W, F, S, OF, R> std::norm(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		return x._real * x._real + x._imag * x._imag;
+	}
+
+	template<std::size_t W, std::size_t F, Fi::Signedness S,
+	         template <typename> class OF,
+	         template <typename> class R>
+	Fi::CFixed<W, F, S, OF, R> std::conj(const Fi::CFixed<W, F, S, OF, R>& x)
+	{
+		CFixed<W,F,S,OF,R> ret();
+		ret._real= x._real;
+		ret._imag= -x._imag;
+		return ret;
+	}
+
+#endif
